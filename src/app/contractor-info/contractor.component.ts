36b4ef47ecdc9eeeea237a44b83bdf023832e114
sol <i.solnur@gmail.com>
2017-08-16 15:32:26 +0600

    added reviews component

--- src/app/contractor-info/contractor.component.ts
+++ src/app/contractor-info/contractor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Location} from '@angular/common';
 
 @Component({
@@ -8,13 +8,48 @@ import {Location} from '@angular/common';
 })
 export class ContractorComponent implements OnInit {
 
-  constructor(private location: Location) { }
+  /**
+   * Vars for hide/show html containers
+   */
+  private infoCont: HTMLElement;
+  private revCont: HTMLElement;
+  private navInfo: HTMLElement;
+  private navRev: HTMLElement;
 
-  ngOnInit() {
+  constructor(private location: Location) {
   }
 
+  ngOnInit() {
+    this.infoCont = document.getElementById('infoCont');
+    this.revCont = document.getElementById('revCont');
+    this.navInfo = document.getElementById('navInfo');
+    this.navRev = document.getElementById('navRev');
+  }
+
+  /**
+   * Handler for navigate back
+   */
   onClickBack() {
     this.location.back();
   }
 
+  /**
+   * Handler for hide/show Info container
+   */
+  onClickInfo() {
+    this.infoCont.style.display = 'block';
+    this.navInfo.style.borderBottom = '3px #2196F3 solid';
+    this.navRev.style.borderBottom = 'none';
+    this.revCont.style.display = 'none';
+  }
+
+  /**
+   * Handler for hide/show Reviews container
+   */
+  onClickRev() {
+    this.revCont.style.display = 'block';
+    this.infoCont.style.display = 'none';
+    this.navInfo.style.border = 'none';
+    this.navRev.style.borderBottom = '3px #2196F3 solid';
+  }
 }
