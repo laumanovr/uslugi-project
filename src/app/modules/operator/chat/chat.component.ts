185a8aa0e151afe18412f721290bdc4ac478e7ec
Melnikov Alexandr <minifets@gmail.com>
2017-10-12 11:35:50 +0600

    Added messages array

--- src/app/modules/operator/chat/chat.component.ts
+++ src/app/modules/operator/chat/chat.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+    selector: 'app-chat',
+    templateUrl: './chat.component.html',
+    styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+    OPERATOR = 'operator';
+    CLIENT = 'client';
 
-  ngOnInit() {
-  }
+    messages = [
+        {id: 1, who: this.OPERATOR, type: 'image', content: '../../../assets/img/antenna.png', date: new Date('2017-10-09 10:15:00')},
+        {id: 2, who: this.CLIENT, type: 'text', content: 'Текст', date: new Date()},
+        {id: 3, who: this.CLIENT, type: 'desc', content: {
+            desc: 'описание',
+            address: 'ул. Медерова, 56',
+            date: '03.08.2017',
+            time: '10:00 - 12:00',
+            price: '1500 сом'
+        }, date: new Date()}
+    ];
 
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    newChat() {
+        console.log('Hear');
+    }
 }
