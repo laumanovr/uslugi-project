88709d9f0f0961a0430596cfb2cbd696b57f3805
Melnikov Alexandr <minifets@gmail.com>
2017-10-20 14:59:55 +0600

    Added websocket connection

--- src/app/modules/operator/chat/chat.component.ts
+++ src/app/modules/operator/chat/chat.component.ts
@@ -10,9 +10,24 @@ import {CommonService} from '../../../services/common.service';
 })
 export class ChatComponent implements OnInit {
 
-    OPERATOR = 'operator';
-    CLIENT = 'client';
+    private webSocketUrl = 'wss://localhost:9515/';
 
+    /**
+     * Project constants for user types (may be client or operator)
+     */
+    OPERATOR    = 'operator';
+    CLIENT      = 'client';
+
+    /**
+     * Constants for user statuses
+     */
+    ONLINE      = 'Онлайн';
+    OFFLINE     = 'Отсутствует';
+    TYPING      = 'Печатает...';
+
+    /**
+     * @type {[{id: string; who: string; type: string; content: string; date: Date}]}
+     */
     messages = [
         {id: 1, who: this.OPERATOR, type: 'image', content: '../../../assets/img/antenna.png', date: new Date('2017-10-09 10:15:00')},
         {id: 2, who: this.CLIENT, type: 'text', content: 'Текст', date: new Date()},
@@ -25,17 +40,55 @@ export class ChatComponent implements OnInit {
         }, date: new Date()}
     ];
 
-    name = 'Виктория';
+    /**
+     * @type {string} Operator name
+     */
+    name    = '';
 
-    status = 'Онлайн';
+    /**
+     * @type {string} Operator text statuses
+     */
+    status  = this.OFFLINE;
 
+    /**
+     * @type {WebSocket}
+     */
+    connection = null;
+
+    /**
+     * @type {number}
+     */
+    private connTries = 0;
+
+    /**
+     * @param common
+     */
     constructor(private common: CommonService) {
     }
 
     ngOnInit() {
+        this.createConnection();
     }
 
-    newChat() {
-        console.log('Hear');
+    createConnection() {
+        const that = this;
+        this.connection = new WebSocket(this.webSocketUrl);
+        this.connection.onopen = function () {
+            that.connTries = 0;
+            console.log('Start new connection');
+        };
+
+        this.connection.onmessage = function (msg) {
+            console.log(msg.data);
+        };
+
+        this.connection.onerror = function (error) {
+            that.connTries += 1;
+            if (that.connTries < 5) {
+                that.createConnection();
+            }
+
+            console.log(error);
+        };
     }
 }
