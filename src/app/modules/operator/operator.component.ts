3f35085a5c5260adf9c71c5b61c9809e7cd4c187
Ramazan <laumanov.r@gmail.com>
2017-10-21 15:50:53 +0600

    added redirect route to chat

--- src/app/modules/operator/operator.component.ts
+++ src/app/modules/operator/operator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-operator',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperatorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router){}
 
   ngOnInit() {
+
+  }
+
+  onCallOperator(){
+    this.router.navigate(['chat'])
   }
 
 }
