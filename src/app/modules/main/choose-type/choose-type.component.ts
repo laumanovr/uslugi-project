4634ef2c8c0995722e8a71de42b9004176909722
Ramazan <laumanov.r@gmail.com>
2017-10-30 12:21:46 +0600

    added order-create in choose-type

--- src/app/modules/main/choose-type/choose-type.component.ts
+++ src/app/modules/main/choose-type/choose-type.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Location} from '@angular/common';
 import {Router} from '@angular/router';
+import {CommonService} from '../../../services/common.service';
 
 @Component({
   selector: 'app-choose-type',
@@ -9,12 +10,15 @@ import {Router} from '@angular/router';
 })
 export class ChooseTypeComponent implements OnInit {
 
+  master;
+
   constructor(private location: Location,
-              private router: Router) {
+              private router: Router, private common: CommonService) {
   }
 
 
   ngOnInit() {
+    this.master = JSON.parse(this.common.storage.getItem('master'));
   }
 
   /**
@@ -28,7 +32,19 @@ export class ChooseTypeComponent implements OnInit {
     this.router.navigate(['master-call']);
   }
 
-  onClickOperator(){
-    this.router.navigate(['chat']);
+  onClickOperator() {
+    this.common.showAttachIcon = true;
+    this.orderCreate();
+  }
+
+  private orderCreate() {
+    const url = 'createorder';
+    const body = '&serviceid=' + this.common.storage.getItem('serviceId') + '&agent=' + this.master.id
+      + '&mobile=' + this.common.storage.getItem('orderPhone');
+    this.common.post(url, body).subscribe(resp => {
+      this.common.currentOrderId = resp.json()[2];
+      this.router.navigate(['chat']);
+      console.log(resp);
+    });
   }
 }
