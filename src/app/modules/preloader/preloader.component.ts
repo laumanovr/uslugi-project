65284d4cfc93dd17ab07ffccc92071eb04614779
Ramazan <laumanov.r@gmail.com>
2017-10-30 12:34:25 +0600

    created preloader

--- src/app/modules/preloader/preloader.component.ts
+++ src/app/modules/preloader/preloader.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'preloader',
-    template: `<img src="../../../../assets/img/antenna.png">`
+    template: `<img src="../../../../assets/img/preloader.gif">`
 })
 export class PreloaderComponent implements OnInit {
     constructor() { }
